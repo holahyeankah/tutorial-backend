457ceb51355b2d42101fdfb471903843a7115e49
Olayinka <holahyeankah12@gmail.com>
2021-09-11 01:13:22 +0100

    adding

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -10,12 +10,17 @@ host: process.env.DB_HOST,
 dialect: "mysql",
 loging:false
   },
-
-
   production: {
-    use_env_variable: 'DATABASE_URL',  
-    dialect: 'mysql'
-  }
+    username: process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    loging:false
+      },
+
+
+
 };
 
 module.exports=config[process.env.NODE_ENV || 'development']
