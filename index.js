6c037945f625f7ed1da88f2ad43d2b8971a04c9c
Olayinka <holahyeankah12@gmail.com>
2021-09-11 01:20:15 +0100

    adding commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const routes =require('./server/Routes/app')
 dotenv.config();
 
 
-const isProduction = process.env.NODE_ENV ==='production'
+
 
 
 const app= express()
@@ -19,50 +19,9 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
- if(!isProduction){
-     app.use(errorhandler())
- }
 app.use(routes)
 
-// catch 404 and forward to error handler
 
-app.use((req, res, next)=>{
-    const err= new Error("Not found")
-    err.status=404;
-    next(err)
-})
-
-
-// error handlers
-
-// development error Handler
-// will print stacktrace
-
- if(!isProduction){
-app.use((err, req, res, next)=>{
-    console.log(err.stack)
-    res.status(err.status || 500);
-    res.json({
-        error:{
-            message:err.message,
-            error:err
-        }
-    })
-
-})
- }
-// production error handler
-// no stacktraces leaked to user
-
- app.use((err, req, res, next)=>{
-     res.status(err.status || 500);
-     res.json({
-         error:{
-             message:err.message,
-             error:{}
-         }
-     })
- })
 
 
 const PORT= process.env.PORT || '3000'
